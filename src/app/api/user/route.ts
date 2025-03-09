a02d22a072cc641999b2235708e5586822bafef1
tejeoner34 <atejezar@gmail.com>
2025-03-09 17:23:20 +0900

    feat: install prisma and sign up end point

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,33 @@
+import { db } from '@/lib/db';
+import { NextResponse } from 'next/server';
+import { hash } from 'bcrypt';
+import { userSchema } from '@/domain/schemas/user.schema';
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    const { email, password } = userSchema.parse(body);
+
+    // Check if the user already exists
+    const existingUserEmail = await db.user.findUnique({ where: { email } });
+    if (existingUserEmail) {
+      return NextResponse.json({ message: 'User already exists', user: null }, { status: 409 });
+    }
+
+    //Create a secure password
+    const hashedPassword = await hash(password, 10);
+    const newUser = await db.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    const { password: newUserPassword, ...rest } = newUser;
+    return NextResponse.json({ user: rest });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: 'Something went wrong', user: null }, { status: 500 });
+  }
+}
