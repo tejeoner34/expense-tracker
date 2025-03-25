8ea066c2d54e150af367e013704211ff0271b77b
tejeoner34 <atejezar@gmail.com>
2025-03-25 12:04:17 +0900

    feat: add dashboard nav to header

--- __tests__/components/ui/Header.component.test.tsx
+++ __tests__/components/ui/Header.component.test.tsx
@@ -2,26 +2,29 @@ import Header from '@/components/ui/Header.component';
 import { render, screen } from '@testing-library/react';
 import { SessionProvider } from 'next-auth/react';
 
-describe('Header Component', () => {
-  it('Should render with correct CSS classes', () => {
-    render(
-      <SessionProvider session={null}>
-        <Header />
-      </SessionProvider>
-    );
-    const header = screen.getByRole('banner');
-    expect(header).toHaveClass('flex', 'justify-end', 'p-4');
-  });
+jest.mock('next/router', () => ({
+  useRouter: jest.fn(),
+}));
 
+describe('Header Component', () => {
   it('Should contain AuthButton component', () => {
     render(
       <SessionProvider session={null}>
         <Header />
       </SessionProvider>
     );
-    const header = screen.getByRole('banner');
-    expect(header.children).toHaveLength(1);
-    expect(header.firstChild).toBeTruthy();
+    const authButton = screen.getByRole('button');
+    expect(authButton).toBeInTheDocument();
+  });
+
+  it('Should contain link to dashboard', () => {
+    render(
+      <SessionProvider session={null}>
+        <Header />
+      </SessionProvider>
+    );
+    const nav = screen.getByRole('navigation');
+    expect(nav).toBeInTheDocument();
   });
 
   it('Should maintain consistent layout structure', () => {
--- src/components/ui/Header.component.tsx
+++ src/components/ui/Header.component.tsx
@@ -1,8 +1,18 @@
+import Link from 'next/link';
 import { AuthButton } from '../auth/AuthButton.component';
+import { routes } from '@/app/routes/routes';
 
 export default function Header() {
   return (
-    <header className="flex justify-end p-4">
+    <header className="flex justify-between items-center p-4">
+      <nav>
+        <Link
+          href={routes.dashboard}
+          className="relative after:content-[''] after:absolute after:w-full after:scale-x-0 after:h-[2px] after:bottom-0 after:left-0 after:bg-black after:origin-right after:transition-transform after:duration-300 hover:after:scale-x-100 hover:after:origin-left"
+        >
+          Dashboard
+        </Link>
+      </nav>
       <AuthButton />
     </header>
   );
