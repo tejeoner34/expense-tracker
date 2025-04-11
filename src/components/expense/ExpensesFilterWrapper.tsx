cb6c86a0c355a9eff965375dd59c02383174d574
tejeoner34 <atejezar@gmail.com>
2025-04-11 11:12:59 +0900

    feat: styled dashboard

--- src/components/expense/ExpensesFilterWrapper.tsx
+++ src/components/expense/ExpensesFilterWrapper.tsx
@@ -19,17 +19,26 @@ const ExpensesFilterWrapper = ({ userId, years }: Props) => {
   });
 
   return (
-    <div>
-      <ExpensesFilters years={years} selectedYear={selectedYear} onFilter={setSelectedYear} />
-      <ExpensesList expensesList={expenses} />
-      <div className="h-96 w-full">
-        <CategoriesChart expensesList={expenses} />
+    <div className="flex flex-col gap-6 p-4 md:p-6">
+      <div className="w-50">
+        <ExpensesFilters years={years} selectedYear={selectedYear} onFilter={setSelectedYear} />
       </div>
-      <div className="h-96 w-full">
-        <MonthlyExpenseChart expensesList={expenses} />
-      </div>
-      <div className="h-96 w-full">
-        <DailyExpenseChart expensesList={expenses} />
+
+      <div className="bg-white shadow rounded-2xl p-4 md:p-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div>
+            <ExpensesList expensesList={expenses} />
+          </div>
+          <div className="h-72 md:h-80 w-full">
+            <CategoriesChart expensesList={expenses} />
+          </div>
+          <div className="h-72 md:h-80 w-full">
+            <MonthlyExpenseChart expensesList={expenses} />
+          </div>
+          <div className="h-72 md:h-80 w-full">
+            <DailyExpenseChart expensesList={expenses} />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/ui/Header.component.tsx
+++ src/components/ui/Header.component.tsx
@@ -1,19 +1,25 @@
 import Link from 'next/link';
 import { AuthButton } from '../auth/AuthButton.component';
 import { routes } from '@/app/routes/routes';
+import { Separator } from './separator';
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center p-4">
-      <nav>
-        <Link
-          href={routes.dashboard}
-          className="relative after:content-[''] after:absolute after:w-full after:scale-x-0 after:h-[2px] after:bottom-0 after:left-0 after:bg-black after:origin-right after:transition-transform after:duration-300 hover:after:scale-x-100 hover:after:origin-left"
-        >
-          Dashboard
-        </Link>
-      </nav>
-      <AuthButton />
+    <header className="sticky top-0 z-50 bg-white">
+      <div className="flex justify-between items-center p-4">
+        <nav>
+          <Link
+            href={routes.dashboard}
+            className="relative after:content-[''] after:absolute after:w-full after:scale-x-0 after:h-[2px] after:bottom-0 after:left-0 after:bg-black after:origin-right after:transition-transform after:duration-300 hover:after:scale-x-100 hover:after:origin-left"
+          >
+            Dashboard
+          </Link>
+        </nav>
+        <AuthButton />
+      </div>
+      <div className="w-[80%] m-auto">
+        <Separator />
+      </div>
     </header>
   );
 }
